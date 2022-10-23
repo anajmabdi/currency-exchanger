a2ea13c0ccbf41a0dd4ec355b089c0e32f14f16e
Najma Abdi <anajmabdi@gmail.com>
2022-10-22 19:24:18 -0700

    add API call and error handling

---
+++ src/convert.js
@@ -0,0 +1,16 @@
+export default class ConvertService {
+  static exchange(baseUSD, newCurrency) {
+    return fetch(`https://v6.exchangerate-api.com/v6/5f227f3853a28e15e39e8e2b/pair/${baseUSD}/${newCurrency}`)
+    .then(function(response) {
+      if(!response.ok) {
+        const errorMessage = `${response.status} ${response.statusTest}`;
+        throw new Error(errorMessage);
+      } else {
+        return response.json();
+      }
+    })
+    .catch(function(error) {
+      return error;
+    });
+  }
+}
